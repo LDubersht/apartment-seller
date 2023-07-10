1aa305fabfc0f6c9712ab63b6786970f5b8b01db
Leonid Dubershtein <ldubersht@gmail.com>
2023-07-10 20:21:18 +0300

    Done

--- apt-filter.js
+++ apt-filter.js
@@ -2,15 +2,17 @@
 
 const must = (radio) => radio.filter(rb => rb.checked).length
 
-const findRelevantApts = function (address, minPrice, maxPrice, minRooms, maxRooms, immediate) {
+const findRelevantApts = function (address, minPrice, maxPrice, minRooms, maxRooms, parking, immediate) {
     let relevantApts = apartments.filter(a =>
         a.address.toLowerCase().includes(address.toLowerCase() || "") &&
         a.price >= (minPrice || 0) &&
         a.price <= (maxPrice || a.price) &&
         a.rooms >= (minRooms || 0) &&
-        a.rooms <= (maxRooms || a.rooms)
+        a.rooms <= (maxRooms || a.rooms) &&
+        immediate ? a.immediate : true &&
+        parking ? a.parking : true
     )
 
-    immediate = !immediate[0].checked
-    return immediate ? relevantApts : relevantApts.filter(a => (a.immediate)) 
+    // immediate = !immediate[0].checked
+    return  relevantApts 
 }
--- main.js
+++ main.js
@@ -5,16 +5,23 @@ $("button").on("click", function () {
     let maxPrice = $("#max-p-input").val()
     let minRooms = $("#min-r-input").val()
     let maxRooms = $("#max-r-input").val()
-    let immediate = $("#immed-y")
+    let immediate = $("#immed-y").is(':checked')
+    let parking = $("#park-y").is(':checked')
 
-    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate)
+    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, parking, immediate)
     renderApts(relevantApts)
 })
 
 const renderApts = function (apartments) {
     $("#results").empty()
     console.log(apartments) //array of apartments to render
-    //Your code goes here.
+    const source = $('#result-template').html();
+    const template = Handlebars.compile(source);
+    
+        let newHTML = template({apartments});
+        $("#results").append(newHTML);
+
+
 }
 
 renderApts(apartments) //renders apartments when page loads
